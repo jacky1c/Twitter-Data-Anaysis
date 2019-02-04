855da0507cd4a345249cf9efcbe9980d6135ed6f
Jacky <jacky@Jackys-MacBook-Pro.local>
2019-02-04 11:51:38 -0600

    Recommit

--- Prototype/twitter data analysis.py
+++ Prototype/twitter data analysis.py
@@ -28,34 +28,34 @@ outfilename = 'superbowl_'+timeStr+'.json'
 # Collect data from Twitter API and export to json file
 collect_twitter_data(outfilename)    
         
-# Load json data from file to python dictionary
-with open('superbowl_2019-02-03_15:32:00.json') as infile:
-    data = json.load(infile)
-
-# Flatten twitter data
-tweets_list = flatten_tweets(data)
-
-# Create a DataFrame from `twitter data
-tweets = pd.DataFrame(tweets_list)
-
-# Print out the first 5 tweets from this dataset
-#print(tweets['text'].values[0:5])
-
-# Get a Series of tweets that contain a keyword in tweet, quoted tweet, or retweet
-#check_word = check_word_in_tweet('ram', tweets)
-#print(np.sum(check_word)/tweets.shape[0])
-
-# Instantiate sentiment intensity analyzer
-sid = SentimentIntensityAnalyzer()
-
-# Generate sentiment scores by applying polarity_scores function to all tweet text
-sentiment_scores = tweets['text'].apply(sid.polarity_scores)
-
-# Extract compound from sentiment score. Options are: neg, neu, pos, compound
-sentiment = sentiment_scores.apply(lambda x: x['compound'])
-
-# Calculate average compound score for tweets that contain keyword
-sentiment_patriots = sentiment[check_word_in_tweet('patriots', tweets)].mean()
-sentiment_ram = sentiment[check_word_in_tweet('ram', tweets)].mean()
-print(sentiment_patriots)
-print(sentiment_ram)
+## Load json data from file to python dictionary
+#with open('superbowl_2019-02-03_15:32:00.json') as infile:
+#    data = json.load(infile)
+#
+## Flatten twitter data
+#tweets_list = flatten_tweets(data)
+#
+## Create a DataFrame from `twitter data
+#tweets = pd.DataFrame(tweets_list)
+#
+## Print out the first 5 tweets from this dataset
+##print(tweets['text'].values[0:5])
+#
+## Get a Series of tweets that contain a keyword in tweet, quoted tweet, or retweet
+##check_word = check_word_in_tweet('ram', tweets)
+##print(np.sum(check_word)/tweets.shape[0])
+#
+## Instantiate sentiment intensity analyzer
+#sid = SentimentIntensityAnalyzer()
+#
+## Generate sentiment scores by applying polarity_scores function to all tweet text
+#sentiment_scores = tweets['text'].apply(sid.polarity_scores)
+#
+## Extract compound from sentiment score. Options are: neg, neu, pos, compound
+#sentiment = sentiment_scores.apply(lambda x: x['compound'])
+#
+## Calculate average compound score for tweets that contain keyword
+#sentiment_patriots = sentiment[check_word_in_tweet('patriots', tweets)].mean()
+#sentiment_ram = sentiment[check_word_in_tweet('ram', tweets)].mean()
+#print(sentiment_patriots)
+#print(sentiment_ram)
